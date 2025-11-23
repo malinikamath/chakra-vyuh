60e1fd1887dce5a598951c31418d8124a8770016
malinik <malinik@umich.edu>
2025-11-22 22:01:56 -0500

    more pathway to left

---
+++ js/navigation_left_one.js
@@ -0,0 +1,20 @@
+// left one path
+let question = document.getElementById("question");
+let notebook = document.querySelector("h2");
+let clickedQuestion = false;
+question.addEventListener("click", function(){
+    clickedQuestion = true;
+    question.innerHTML = "... is what you'd think if you were delusional.<p>When you stand again, it's obvious that something isn't right here, especially when you spot a dark hallway ahead that certainly wasn't there before. You decide to try opening the Notebook. It deserves capitalizing it's name, when it's so important.</p>";
+    let entry = document.getElementById("left_one_notebook");
+    entry.setAttribute('data-title', "You're sharp if you found this hint. Tip: don't trust everything you see. Besides this notebook, obviously. <p><span class='hidden_text'>*Wrong notebook.</span><p>");
+    notebook.innerHTML = "Notebook";
+})
+
+notebook.addEventListener("click", function(){
+    if(clickedQuestion == true){
+        let content = document.getElementById("notebook");
+        content.style.margin = 0;
+        content.innerHTML = notebook.inner = '<a class= "btn" href="hallway_one.html">RUN DOWN DARK HALLWAY</a>';
+        question.innerHTML = "... or you will be if you keep moving.<br><br>The Notebook said to run down the hallway, didn't it?";
+    }
+})
